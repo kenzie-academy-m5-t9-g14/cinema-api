5f6046b2b9979b82a2048ffc5be731068ff4372d
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-12 11:06:36 -0300

    feat: create Genre model

--- genres/models.py
+++ genres/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Genre(models.Model):
+    id = models.UUIDField(
+            primary_key = True, 
+            default = uuid.uuid4,
+            editable = False
+         )
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
