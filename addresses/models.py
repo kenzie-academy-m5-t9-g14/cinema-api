67158fde20a132233a9026efff4cc89398fb6b8b
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-12 10:44:56 -0300

    feat: create Address model

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    id = models.UUIDField(
+            primary_key = True, 
+            default = uuid.uuid4,
+            editable = False
+         )
+    street = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    number = models.IntegerField()
+    city = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=7)
