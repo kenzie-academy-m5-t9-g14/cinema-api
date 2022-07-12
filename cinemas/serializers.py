010e2b8885f1c4f5c3c23d6f0fe635a8d3972b9b
Paulo Guarnieri <91763452+pauloguarnieri@users.noreply.github.com>
2022-07-12 15:05:22 -0300

    Delete cinemas directory

--- cinemas/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from cinemas.models import Cinema
-
-class CinemaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cinema
-        fields = "__all__"
