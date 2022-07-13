ff664d5a8cee6d754323f0b6cd5b503fcd3c53cb
GabrielRD <grd342@outlook.com>
2022-07-13 02:04:54 -0300

    bug:bug_on_UUID-need-to-fix

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from movies.models import MoviesModel
-
+from genres.serializers import GenreSerializer
 
 class MovieSerializer(serializers.ModelSerializer):
-
+    genres = GenreSerializer(read_only = True)
     class Meta:
         model = MoviesModel
-        fields = "__all__"
+        # fields = "__all__"
+        fields = ["id","name","duration","parental_rating","sinopse","imdb_rating","release_date","closing_date","created_at","updated_at","genres"]
+        extra_kwargs = {"id":{"read_only":True},"created_at":{"read_only":True}, "updated_at":{"read_only":True},"genres":{"read_only":True}}
+        # depth = 1
         
--- movies/views.py
+++ movies/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from . import models
 from . import serializers
-
-
+from genres.models import Genre
+from django.forms.models import model_to_dict
 
 
 
@@ -12,6 +12,15 @@ class MovieView(generics.ListCreateAPIView):
     serializer_class = serializers.MovieSerializer
 
 
+    def perform_create(self, serializer):
+        genres_capture = self.request.data.get("genres")
+        new_genre = Genre.objects.get_or_create(name=genres_capture)[0]
+        dicionario = {"id":new_genre.id,"name":new_genre.name}
+        print(dicionario)
+
+
+        serializer.save(genres = dicionario)
 class MovieViewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.MoviesModel.objects.all()
     serializer_class = serializers.MovieSerializer
+
