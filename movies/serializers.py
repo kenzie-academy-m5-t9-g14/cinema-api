b9d7efb2031bc594322bb56b937f2cc2d59375b9
GabrielRD <grd342@outlook.com>
2022-07-13 12:47:36 -0300

    fix: bug-UUID fixed

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers
 from movies.models import MoviesModel
 from genres.serializers import GenreSerializer
+from genres.models import Genre
+import ipdb
+
+
 
 class MovieSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(read_only = True)
+    genres = GenreSerializer(many=True)
+
     class Meta:
         model = MoviesModel
         # fields = "__all__"
         fields = ["id","name","duration","parental_rating","sinopse","imdb_rating","release_date","closing_date","created_at","updated_at","genres"]
-        extra_kwargs = {"id":{"read_only":True},"created_at":{"read_only":True}, "updated_at":{"read_only":True},"genres":{"read_only":True}}
+        extra_kwargs = {"id":{"read_only":True},"created_at":{"read_only":True}, "updated_at":{"read_only":True}}
         # depth = 1
         
+
+
+    def create(self,validated_data):
+        genre = validated_data.pop("genres")
+
+        new_genre = [Genre.objects.get_or_create(name=item["name"])[0] for item in genre]
+
+        movie = MoviesModel.objects.create(**validated_data)
+
+        ipdb.set_trace()
+        movie.genres.set(new_genre)
+
+        return movie
--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,7 @@ from . import models
 from . import serializers
 from genres.models import Genre
 from django.forms.models import model_to_dict
-
+import ipdb
 
 
 class MovieView(generics.ListCreateAPIView):
@@ -12,15 +12,22 @@ class MovieView(generics.ListCreateAPIView):
     serializer_class = serializers.MovieSerializer
 
 
-    def perform_create(self, serializer):
-        genres_capture = self.request.data.get("genres")
-        new_genre = Genre.objects.get_or_create(name=genres_capture)[0]
-        dicionario = {"id":new_genre.id,"name":new_genre.name}
-        print(dicionario)
+    # def perform_create(self, serializer):
+    #     genres_capture = self.request.data.get("genres")
+    #     # for item in genres_capture:
+    #         # new_genre = Genre.objects.get_or_create(name=item.name)[0]
+    #     # # new_genre = Genre.objects.get_or_create(name=genres_capture["name"])[0]
+    #     # dicionario = {"id":new_genre.id,"name":new_genre.name}
+    #     # print(dicionario)
+    #     # new_genre = [Genre.objects.get_or_create(name=item["name"])[0] for item in genres_capture]
+    #     # ipdb.set_trace()
 
-
-        serializer.save(genres = dicionario)
+    #     # serializer.save(genres=new_genre)
 class MovieViewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.MoviesModel.objects.all()
     serializer_class = serializers.MovieSerializer
 
+
+
+# To do:
+# alterar a forma que eu recebo miha requisição
