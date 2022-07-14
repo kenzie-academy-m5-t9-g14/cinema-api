0c2ebcd0c2a26314abe71013e4a10b949d6126e5
Paulo Guarnieri <91763452+pauloguarnieri@users.noreply.github.com>
2022-07-14 18:02:11 -0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=50, null=False)
     cpf = models.CharField(max_length=11, null=False)
     email = models.EmailField(unique=True, null=False)
-    password = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=100, null=False)
     birth_date = models.DateField(null=False)
     cellphone = models.CharField(max_length=13, null=True)
     genre = models.CharField(max_length=20, null=True)
