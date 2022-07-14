a9e401b92335a0568f00848353f7847dc711527b
Paulo Guarnieri <paulo.r.guarnieri@hotmail.com>
2022-07-14 19:12:48 -0300

    test

--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api-kinema.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
