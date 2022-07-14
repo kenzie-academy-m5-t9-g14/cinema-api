75d602b5f2be4bf4d186289bff4794355591b9ab
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-14 14:28:08 -0300

    feat: add view to show movie_sessions of movie_theaters

--- movie_theaters/views.py
+++ movie_theaters/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics
 
-from movie_theaters.serializers import MovieTheaterSerializer
+from movie_theaters.serializers import MovieTheaterSerializer, MovieTheaterMovieSessionsSerializer
 from .models import MovieTheater
 
-
+from movie_sessions.models import MovieSession
+from movie_sessions.serializers import MovieSessionSerializer
 
 class MovieTheaterView(generics.ListCreateAPIView):
     queryset = MovieTheater.objects.all()
@@ -13,3 +14,8 @@ class MovieTheaterView(generics.ListCreateAPIView):
 class MovieTheaterDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MovieTheater.objects.all()
     serializer_class = MovieTheaterSerializer
+
+
+class MovieTheaterMovieSessionsView(generics.ListCreateAPIView):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieTheaterMovieSessionsSerializer
