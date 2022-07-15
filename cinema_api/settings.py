e33c47e64030ab80b4b1801f9e8620d3454f3209
Paulo Guarnieri <paulo.r.guarnieri@hotmail.com>
2022-07-15 10:20:51 -0300

    fix: allowed host

--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['api-kinema.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
