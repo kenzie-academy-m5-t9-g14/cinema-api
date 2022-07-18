9a52fd7671cc05c55f48520e721e8a8f875dc718
GabrielRD <grd342@outlook.com>
2022-07-18 19:13:35 -0300

    feat: movies_tests

--- users/models.py
+++ users/models.py
@@ -22,7 +22,7 @@ class User(AbstractUser):
 
     username = models.CharField(unique=False, null=True, max_length=255)
 
-    address = models.ForeignKey('addresses.Address', on_delete=models.DO_NOTHING, related_name='users', null=True)
+    address = models.ForeignKey('addresses.Address', on_delete=models.DO_NOTHING, related_name='users')
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'cpf', 'birth_date',]
