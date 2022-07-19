e14e6f81f75dbc1907b9034ba20ad82eea5198e4
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 20:12:59 -0300

    feat: create Avaible Seats url

--- seats/urls.py
+++ seats/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SeatView,SeatDetailView
+from .views import SeatView,SeatDetailView, SeatSessionView
 
 urlpatterns = [
     path("movie_theater/<pk>/seats/",SeatView.as_view()),
-    path("seats/<int:pk>/",SeatDetailView.as_view())
+    path("seats/<int:pk>/",SeatDetailView.as_view()),
+    path("movie_sessions/<str:movie_session_id>/seats/",SeatSessionView.as_view())
 ]
