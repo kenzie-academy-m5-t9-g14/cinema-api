5d17c6903f4c9d486bcd49133ca4f69624c4af6c
Daniele Calixto <danielecalixtob@gmail.com>
2022-07-19 17:38:44 -0300

    feat: create serializers avaible seats url

--- movie_sessions/serializers.py
+++ movie_sessions/serializers.py
@@ -5,7 +5,9 @@ from movies.models import MoviesModel
 from movies.serializers import MovieSerializer
 from schedules.models import Schedule
 from schedules.serializers import ScheduleSerializer
+from seats.models import Seat
 from .models import MovieSession
+
 import ipdb
 
 class MovieSessionSerializer(serializers.ModelSerializer):
@@ -28,6 +30,18 @@ class MovieSessionSerializer(serializers.ModelSerializer):
            movie_session.schedule.set(schedule_instances)
            return movie_session
 
+#class SeatsAvaibleSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Seat
+#        fields = ['id', 'row', 'seat']
+#
+#
+#class MovieSessionSeatsSerializer(serializers.ModelSerializer):
+#    avaible_seats = SeatsAvaibleSerializer(many=True)
+#    
+#    class Meta:
+#        model = MovieSession
+#        fields = ['avaible_seats']
     
 
 
